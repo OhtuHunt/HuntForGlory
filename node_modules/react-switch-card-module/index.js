a3c49ebb721d161dd615c7b13a47e7839e9e9c73
Ouzii <oskari.laaja@gmail.com>
2018-01-22 17:43:06 +0200

    Started planning the quest listing site

---
+++ node_modules/react-switch-card-module/index.js
@@ -0,0 +1,7 @@
+/**
+ * @authors  (duke 365650070@qq.com)
+ * @date    2016-11-21 11:44:01
+ * @version $Id$
+ */
+ 
+module.exports = require('./src/');
--- src/index.js
+++ src/index.js
@@ -2,47 +2,27 @@ import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Card, CardHeader, CardBody, CardFooter } from "react-simple-card";
 
-const Quest = (props) => {
-
-    const { quests } = props
-    const lines = () => quests.map(quest => <a href="localhost:3000"><li key={quest.id}>{quest.content} {quest.done}</li></a>)
-
+const GenerateCard = ({ title, text }) => {
     return (
         <div>
-            <h1>Tehtävät</h1>
-            <ul>
-                {lines()}
-            </ul>
+        <Card>
+            <CardBody>
+                <h2>{title}</h2>
+                <p>{text}</p>
+            </CardBody>
+        </Card>
         </div>
     )
 }
-
-const App = () => {
-    const quests = [
-        {
-            id: 1,
-            content: 'Käy laitoksella',
-            done: true
-        },
-        {
-            id: 2,
-            content: 'Syö lounasta',
-            done: false
-        },
-        {
-            id: 3,
-            content: 'Moikkaa kurssikaveria',
-            done: true
-        },
-    ]  
-
-    return (
-        <div>
-            <Quest quests={quests} />
-        </div>
-    )
-}
+const App = () => (
+    <div>
+        <GenerateCard title="Käy laitoksella" text="Käy laitoksella huoneessa BK112 ja anna taululla lukeva koodi" />
+        <GenerateCard title="Käy lounaalla" text="Käy laitoksella kanssaopiskelijan kanssa" />
+        <GenerateCard title="Anna Artolle yläfemma" text="You know what to do!" />
+    </div>
+);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
