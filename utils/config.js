11b58990b10786b4d3e58ecc3829ae7c40e9b2a0
Ouzii <oskari.laaja@gmail.com>
2018-02-19 18:15:40 +0200

    Added config for staging server

--- utils/config.js
+++ utils/config.js
@@ -4,7 +4,9 @@ if (process.env.NODE_ENV !== 'production') {
 
 let port = process.env.PORT
 let mongoUrl = process.env.MONGODB_URI
-
+if (process.env.NODE_ENV === 'development') {
+    mongoUrl = process.env.DEV_MONGODB_URI
+}
 if (process.env.NODE_ENV === 'test') {
     port = process.env.TEST_PORT
     mongoUrl = process.env.TEST_MONGODB_URI
