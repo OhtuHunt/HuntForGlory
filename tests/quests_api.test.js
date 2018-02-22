8e387211d883686dfdc8816be4d44f8cb30de0b7
Ouzii <oskari.laaja@gmail.com>
2018-02-22 16:03:06 +0200

    Testing if travis fails to async-syntax

--- tests/quests_api.test.js
+++ tests/quests_api.test.js
@@ -4,18 +4,18 @@ const { initialQuests, questsInTestDb } = require('./test_helper')
 const { app, server } = require('../src/server/server')
 const api = supertest(app)
 
-beforeAll(async () => {
+beforeAll(() => {
 
     // Be sure to use test database
-    await Quest.remove({})
+    Quest.remove({})
 
     const questObjects = initialQuests.map(quest => new Quest(quest))
     const promiseArray = questObjects.map(quest => quest.save())
-    await Promise.all(promiseArray)
+    Promise.all(promiseArray)
 })
 
-test('quests are returned as json', async () => {
-    await api
+test('quests are returned as json', () => {
+    api
       .get('/api/quests')
       .expect(200)
       .expect('Content-Type', /application\/json/)
