fea6f24156ac6cb7853b1a255037b466b153cb33
sambo1111 <sami.hirvonen3@gmail.com>
2018-02-26 16:01:31 +0200

    Fixed loginRouter to create new user if user not found in database

--- controllers/login.js
+++ controllers/login.js
@@ -1,6 +1,7 @@
 const loginRouter = require('express').Router()
 const AppUser = require('../models/app_user')
 const tmcAuth = require('../utils/tmcAuth')
+const jwt = require('jsonwebtoken')
 
 require('es6-promise').polyfill();
 const fetch = require('isomorphic-fetch');
@@ -17,9 +18,18 @@ loginRouter.post('/', async (request, response) => {
     }
     try {
         const res = await tmcClient.authenticate(param)
-        const userById = await tmcAuth.authenticate(res.accessToken)
+        const config = {
+            headers: {
+                'Authorization': `Bearer ${res.accessToken}`,
+                'Content-Type': 'application/json'
+            }
+        }
+
+        const user = await axios.get('https://tmc.mooc.fi/api/v8/users/current', config)
+        let userById = await AppUser.findOne({ "tmc_id": user.data.id })
 
         if (userById === null) {
+            
             const appUser = new AppUser({
                 username: user.data.username,
                 email: user.data.email,
@@ -29,11 +39,10 @@ loginRouter.post('/', async (request, response) => {
             })
 
             const userToSave = await appUser.save()
-            response.status(200).send({user: userToSave, token: res.accessToken})
-        } else {
-            console.log({user: userById, token: res.accessToken})
-            response.status(200).send({user: userById, token: res.accessToken})
-        }
+            userById = await AppUser.findOne({ "tmc_id": userToSave.tmc_id })
+        } 
+
+        response.status(200).send({user: userById, token: res.accessToken})
 
     } catch (e) {
         console.error(e);
