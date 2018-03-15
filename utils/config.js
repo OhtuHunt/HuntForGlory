36f18dca15c3afc06ec9693147dc1ec9ab55f54a
Ouzii <oskari.laaja@gmail.com>
2018-03-15 15:46:05 +0200

    Rebuilt frontend, implemented logic to support local backend for development purposes

--- utils/config.js
+++ utils/config.js
@@ -1,9 +1,9 @@
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
-}
+
+require('dotenv').config()
 
 let port = process.env.PORT
 let mongoUrl = process.env.MONGODB_URI
+
 if (process.env.NODE_ENV === 'development') {
     mongoUrl = process.env.DEV_MONGODB_URI
 }
