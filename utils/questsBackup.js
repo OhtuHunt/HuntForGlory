d41a799ded773a60c5e75489e76c829e4b433b68
teepiik <teemu.piik@gmail.com>
2018-04-08 19:35:49 +0300

    quests for test launch added in separate file

---
+++ utils/questsBackup.js
@@ -0,0 +1,108 @@
+const quests = [
+    {
+        name: "Osallistu aloitusluennolle",
+        description: "Osallistu kurssin OTM aloitusluennolle",
+        points: 0,
+        type: "solo quest",
+        activationCode: "code"
+        
+    },
+    {
+        name: "Käy pajassa",
+        description: "Käy missä tahansa kurssin pajassa",
+        points: 0,
+        type: "solo quest",
+        activationCode: "code"
+        
+    },
+    {
+        name: "Pidä lukupiiri muiden oppilaiden kanssa",
+        description: "Järjestäkää kurssin aikana lukupiiri noin 4-10 opiskelijan kanssa ja kertokaa assarille",
+        points: 0,
+        type: "Team quest",
+        activationCode: "code"
+        
+    },
+    {
+        name: "Palauta viikon 1 tehtävät",
+        description: "Palauta viikon 1 tehtävät laskarissa, saat assarilta koodin",
+        points: 0,
+        type: "Solo quest",
+        activationCode: "code"
+        
+    },
+    {
+        name: "Palauta viikon 2 tehtävät",
+        description: "Palauta viikon 2 tehtävät laskarissa, saat assarilta koodin",
+        points: 0,
+        type: "Solo quest",
+        activationCode: "code"
+        
+    },
+    {
+        name: "Palauta viikon 3 tehtävät",
+        description: "Palauta viikon 3 tehtävät laskarissa, saat assarilta koodin",
+        points: 0,
+        type: "Solo quest",
+        activationCode: "code"
+        
+    },
+    {
+        name: "Palauta viikon 4 tehtävät",
+        description: "Palauta viikon 4 tehtävät laskarissa, saat assarilta koodin",
+        points: 0,
+        type: "Solo quest",
+        activationCode: "code"
+        
+    },
+    {
+        name: "Palauta viikon 5 tehtävät",
+        description: "Palauta viikon 5 tehtävät laskarissa, saat assarilta koodin",
+        points: 0,
+        type: "Solo quest",
+        activationCode: "code"
+        
+    },
+    {
+        name: "Palauta viikon 6 tehtävät",
+        description: "Palauta viikon 6 tehtävät laskarissa, saat assarilta koodin",
+        points: 0,
+        type: "Solo quest",
+        activationCode: "code"
+        
+    },
+    {
+        name: "Vertaisarvio",
+        description: "Tee vertaisarviointi",
+        points: 0,
+        type: "Solo quest",
+        activationCode: "code"
+        
+    },
+    {
+        name: "Anna kurssipalautetta",
+        description: "Täytä kurssipalautelomake",
+        points: 0,
+        type: "Solo quest",
+        activationCode: "code"
+        
+    },
+    {
+        name: "Osallistu Demoon",
+        description: "Osallistu demotilaisuuteen",
+        points: 0,
+        type: "Solo quest",
+        activationCode: "code"
+        
+    },
+    {
+        name: "Loppupalautus",
+        description: "Tee loppupalautus",
+        points: 0,
+        type: "Solo quest",
+        activationCode: "code"
+        
+    }
+]
+    
+
